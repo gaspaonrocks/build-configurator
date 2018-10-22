5437c9affca735cb70886b62ec394f162372c00a
sacha.grasso <sacha.grasso@ext.cdiscount.com>
2018-10-22 16:21:33 +0200

    second commit, first tests, needs more cleanning

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build Configurator
+
+A module that takes care of the building process for you. You configure your build in a simple  declarative way and you reap the benefit of a optimized Webpack build.
+
+## And then...
+
+_To be continued_
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { mergeDeepRight, reduce } from 'ramda';
-import webpack from 'webpack';
+import * as webpack from 'webpack';
 
 class Configurator {
     constructor(
@@ -13,28 +13,35 @@ class Configurator {
      * @param {string} type the type of repo that the config is targeted to
      * @returns {object} the final config object
      */
-    getConfig(type: string = LIB_CONFIG_TYPE): object {
-        const { entry, module, output, plugins } = this.config;
-        if (!entry || !module || !output || !plugins) this.setConfig(type);
+    getConfig(type?: string): object {
+        //const { entry, module, output, plugins } = this.config;
+        //if (!entry || !module || !output || !plugins) this.setConfig(type);
+
+        this.config = {
+            entry: 'src.index.js',
+            output: 'build',
+            module: {},
+            plugins: []
+        }
 
         return this.config;
     }
 
     /**
-       * @description configReducer
-       * @param {Array<object>} configsArray an array containing one or several config objects to merge
-       * @returns {object} the resulting new config object
-       */
+     * @description configReducer
+     * @param {Array<object>} configsArray an array containing one or several config objects to merge
+     * @returns {object} the resulting new config object
+     */
     configReducer(configsArray: Array<object>): object {
         return reduce(mergeDeepRight, this.config, configsArray);
     }
 
     /**
-       * @description setConfig
-       * @param {string} type the type of repo that the config is targeted to
-       * @returns {undefined} styore the config object in this.config
-       */
-    setConfig(type: string = LIB_CONFIG_TYPE): void {
+     * @description setConfig
+     * @param {string} type the type of repo that the config is targeted to
+     * @returns {undefined} styore the config object in this.config
+     */
+    /* setConfig(type: string): void {
         const { commonConfig, fragmentConfig, multiRepoConfig, optimization } = configs;
 
         const configReducer = configsArray => reduce(mergeDeepRight, this.config, configsArray);
@@ -52,7 +59,7 @@ class Configurator {
             default:
                 this.config = {};
         }
-    }
+    } */
 
     /**
      * @description addConfig
@@ -60,7 +67,7 @@ class Configurator {
      * @param {object | array} value the new value passed to the config object
      * @returns {instance} itself to chain method calls
      */
-    addConfig(key: string, value: Array<number | object | string> | number | object | string): Configurator {
+    /* addConfig(key: string, value: Array<number | object | string> | number | object | string): Configurator {
         const { error } = console;
 
         if (!key || !value) {
@@ -75,7 +82,7 @@ class Configurator {
         this.config = mergeDeepRight(this.config, { [key]: value });
 
         return this;
-    }
+    } */
 
     /**
      * @description runConfig
@@ -97,7 +104,7 @@ class Configurator {
     }
 
     plug(key: Array<string> | string): Configurator {
-        if (!Array.isArray(key)) this.config = this.configReducer([configList[key]])
+        if (!Array.isArray(key)) {/* this.config = this.configReducer([configList[key]]) */ }
         else if (Array.isArray(key)) {/* functionnal method to iterate on key */ }
 
         return this;
@@ -109,4 +116,4 @@ class Configurator {
 }
 
 export default new Configurator();
-export { LIB_CONFIG_TYPE, APP_CONFIG_TYPE, FRAGMENT_CONFIG_TYPE };
+// export { LIB_CONFIG_TYPE, APP_CONFIG_TYPE, FRAGMENT_CONFIG_TYPE };
